aa0cf985bd29820c39763eef28fc32de5973f8d1
Jon Lamb <jon@auxon.io>
2023-03-10 07:09:21 -0800

    Add demo workflow

--- freertos/run.sh
+++ freertos/run.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 xauth=/tmp/.docker.xauth
-if [ ! -f "$xauth" ]; then
+if [ ! -v DOCKER_WITHOUT_NVIDIA ] && [ ! -f "$xauth" ]; then
     xauth_list=$(sed -e 's/^..../ffff/' <<< "$(xauth nlist $DISPLAY)")
     if [ ! -z "$xauth_list" ]; then
         echo "$xauth_list" | xauth -f "$xauth" nmerge -
@@ -14,16 +14,18 @@ if [ ! -f "$xauth" ]; then
 fi
 
 DOCKER_OPTS="${DOCKER_OPTS:-}"
-docker_version=$(docker version --format '{{.Server.Version}}')
-if dpkg --compare-versions 19.03 gt "$docker_version" ; then
-    echo "Docker version is less than 19.03, using nvidia-docker2 runtime"
-    if [ ! dpkg --list | grep nvidia-docker2 ]; then
-        echo "Update docker to a version greater than 19.03 or install nvidia-docker2"
-        exit 1
+if [ ! -v DOCKER_WITHOUT_NVIDIA ]; then
+    docker_version=$(docker version --format '{{.Server.Version}}')
+    if dpkg --compare-versions 19.03 gt "$docker_version" ; then
+        echo "Docker version is less than 19.03, using nvidia-docker2 runtime"
+        if [ ! dpkg --list | grep nvidia-docker2 ]; then
+            echo "Update docker to a version greater than 19.03 or install nvidia-docker2"
+            exit 1
+        fi
+        DOCKER_OPTS="$DOCKER_OPTS --runtime=nvidia"
+    else
+        DOCKER_OPTS="$DOCKER_OPTS --gpus all"
     fi
-    DOCKER_OPTS="$DOCKER_OPTS --runtime=nvidia"
-else
-    DOCKER_OPTS="$DOCKER_OPTS --gpus all"
 fi
 
 container_image=${1:-demo-freertos}
